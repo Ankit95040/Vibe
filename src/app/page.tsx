bd516a27fe1cd586175f0554d25baef9c7615a9b
Ankit <asrsingh95040@gmail.com>
2025-12-14 14:11:40 +0530

    03:Trpc SetUp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+
 import React from 'react'
-import { Button } from '@/components/ui/button'
-function page() {
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { getQueryClient, trpc } from '@/trpc/server'
+import {Client} from './client'
+import { Suspense } from 'react'
+
+const page = async () => {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: 'ankit' }));
   return (
-    <div className='text-red-500' >
-      <Button variant='primary'>Click me</Button>
-      hello world
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+      <Client/>
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
---
+++ src/trpc/types.ts
@@ -0,0 +1,3 @@
+// Type-only exports that can be safely imported by client code
+export type { AppRouter } from './routers/_app';
+
